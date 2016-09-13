a15f6de1689020c879340966107e3ebf921a3cf9
SamuelLozanoMartin <lozanomartinsamuel@gmail.com>
2016-09-13 20:51:51 +0200

    lecciones sobre constantes y control de flujo

---
+++ arrayDimensional.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+  define("stock",20);
+for ($row = 0; $row < 4; $row++) {
+	if ($cars[$row][1]<stock){
+		echo "<p><b>Row number $row</b></p>";
+  		echo "<ul>";
+  		for ($col = 0; $col < 3; $col++) {
+    		echo "<li>".$cars[$row][$col]."</li>";
+  		}
+  		echo "</ul>";
+	}
+  
+}
+?>
+
+</body>
+</html>
