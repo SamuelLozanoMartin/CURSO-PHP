16273f39144b1329ad91f1c825b774068f9cc28a
SamuelLozanoMartin <lozanomartinsamuel@gmail.com>
2016-09-29 20:40:28 +0200

    otro ejemplo de mini MVC

---
+++ miniMVC/view.php
@@ -0,0 +1,52 @@
+<?php
+require ("models.php");
+function set_identificadores($vista) {
+	$identificadores = array();
+	if($vista) {
+		switch ($vista) {
+			case 'vista_1':
+				$identificadores = array('propiedad');
+				break;
+			case 'vista_2':
+				$identificadores = array('propiedad_1', 'propiedad_2');
+				break;
+		}
+	return $identificadores;
+	}
+}
+function armar_diccionario($vista, $data) {
+	$diccionario = array();
+	$identificadores = set_identificadores($vista);
+	if($identificadores) {
+		foreach ($identificadores as $identificador) {
+			if(array_key_exists($identificador, $data)) {
+				$diccionario[$identificador] = $data[$identificador];
+			}
+		}
+	}
+	return $diccionario;
+}
+function render_data($vista, $data) {
+	$html = '';
+	if(($vista)&&($data)) {
+		$diccionario = armar_diccionario($vista, $data);
+		if($diccionario) {
+			$html = file_get_contents('html/'.$vista.'.html');
+			foreach ($diccionario as $clave=>$valor) {
+				$html = str_replace('{'.$clave.'}', $valor, $html);
+			}
+		}
+	}
+	print $html;
+}
+/*$data = new ModeloUno();
+$data->a(25);
+settype($data, 'array');
+render_data("vista_1",$data);
+
+$data=new ModeloDos();
+$data->b(25,35);
+settype($data, 'array');
+render_data("vista_2",$data);*/
+enviar_data();
+?>
