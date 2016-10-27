43a754af7fb7a47c708c73797755f240152f200a
SamuelLozanoMartin <lozanomartinsamuel@gmail.com>
2016-10-27 15:32:12 +0200

    comienzo de asignaciones
    proyecto de symfony asignaciones

---
+++ SYMFONY/asignaciones/src/EMM/UserBundle/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EMM\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+        return new response ('Bienvenido a mi modulo de usuarios');
+    }
+
+
+}
